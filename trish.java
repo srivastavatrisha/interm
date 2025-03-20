e06fd162219a4d159fdef922a645da48b7b211cf
Trisha srivastava <123794254+srivastavatrisha@users.noreply.github.com>
2025-03-20 22:07:41 +0530

    Add files via upload

---
+++ trish.java
@@ -0,0 +1,17 @@
+class Pen{
+    String color;
+    String type; //ballpoint; gel pen
+
+    public void write() {
+        System.out.println("writing something");
+    }
+}
+public class OOPS{
+    public static void main(String args[]){
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+    }
+}
